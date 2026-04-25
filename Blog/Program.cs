21cb2bbf3513795cb77a605193ad562f1198ab09
solak1 <rohanasdf12@gmail.com>
2026-04-25 22:45:17 +0800

    fix prerender

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -2,6 +2,7 @@ using Blog;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.Localization;
+using MudBlazor.Services;
 using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,10 +10,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 
 var requestCulture = new RequestCulture("en-us", "en-us");
 
-CultureInfo.DefaultThreadCurrentCulture = requestCulture.UICulture;         // WebAssembly Culture°BUICulture •≤∂∑≠n§@ºÀ https://docs.microsoft.com/zh-tw/aspnet/core/blazor/globalization-localization?view=aspnetcore-6.0&pivots=webassembly
+CultureInfo.DefaultThreadCurrentCulture = requestCulture.UICulture;         // WebAssembly Culture„ÄÅUICulture ÂøÖÈ†àË¶Å‰∏ÄÊ®£ https://docs.microsoft.com/zh-tw/aspnet/core/blazor/globalization-localization?view=aspnetcore-6.0&pivots=webassembly
 CultureInfo.DefaultThreadCurrentUICulture = requestCulture.UICulture;      
 var a = CookieRequestCultureProvider.DefaultCookieName;
 await builder.Build().RunAsync();
